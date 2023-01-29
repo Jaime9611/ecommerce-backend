429e3e851af2ebf1ee798d16786c329bcdfb8664
Jaime Blanco <jaimeescobar9611@gmail.com>
2023-01-29 18:36:14 -0500

    Added product by id endpoint

--- src/main/java/com/example/ecommerce/controllers/v1/ProductController.java
+++ src/main/java/com/example/ecommerce/controllers/v1/ProductController.java
@@ -5,6 +5,8 @@ import com.example.ecommerce.services.ProductService;
 import com.example.ecommerce.utils.constants.Endpoints;
 import java.util.List;
 import java.util.UUID;
+
+import com.example.ecommerce.utils.constants.Paths;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,8 +23,13 @@ public class ProductController {
   @Autowired
   private ProductService productService;
 
-  @GetMapping("/{categoryId}")
-  public ResponseEntity<List<ProductDTO>> getAllProductsbyCategory(
+  @GetMapping(Paths.Products.GET_PRODUCT_BY_ID)
+  public ResponseEntity<ProductDTO> getProductById(@PathVariable UUID productId) {
+    return ResponseEntity.ok(productService.getProductById(productId));
+  }
+
+  @GetMapping(Paths.Products.GET_PRODUCT_BY_CATEGORY)
+  public ResponseEntity<List<ProductDTO>> getAllProductsByCategory(
       @PathVariable UUID categoryId) {
     return ResponseEntity.ok(productService.getAllProductsByCategoryId(categoryId));
   }
--- src/main/java/com/example/ecommerce/utils/constants/Paths.java
+++ src/main/java/com/example/ecommerce/utils/constants/Paths.java
@@ -5,4 +5,11 @@ public class Paths {
   // Auth Paths
   public static final String AUTHENTICATE = "/authenticate";
 
+  // Product Paths
+  public static class Products {
+    public static final String GET_PRODUCT_BY_ID = "product/{productId}";
+    public static final String GET_PRODUCT_BY_CATEGORY = "category/{categoryId}";
+  }
+
+
 }
