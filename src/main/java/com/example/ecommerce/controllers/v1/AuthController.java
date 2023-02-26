390a1bbcf58c435cdf6489619e53d36924b09a2f
Jaime Blanco <jaimeescobar9611@gmail.com>
2023-02-26 13:37:15 -0500

    Removed Cross origin from auth endpoint

--- src/main/java/com/example/ecommerce/controllers/v1/AuthController.java
+++ src/main/java/com/example/ecommerce/controllers/v1/AuthController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(Endpoints.AUTH_ENDPOINT)
-@CrossOrigin(origins = "${react.origin.url}")
 public class AuthController {
 
   @Autowired
